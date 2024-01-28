0e4c48e1c5e7c266294397a792662eb466e50c66
Женечек Ву <53404220+J3ne4ek@users.noreply.github.com>
2024-01-29 00:08:02 +0100

    refactor code

--- diffusion.py
+++ diffusion.py
@@ -3,73 +3,78 @@ import torch.nn as nn
 import torch.nn.functional as F
 from tqdm import tqdm
 import numpy as np
+from enum import Enum
+
+
+class TargetType(Enum):
+    NOISE = 1
+    X_START = 2
 
 
 class DiffusionModel:
-    def __init__(self, model, img_size, n_steps=1000, predict_noise=True, min_beta=1e-4, max_beta=0.02):
+    def __init__(self, model, img_size, n_steps=1000, model_type=TargetType.NOISE, is_conditioned=False,
+                 min_beta=1e-4, max_beta=0.02):
         self.n_steps = n_steps
         self.img_size = img_size
-        self.predict_noise = predict_noise
+        self.type = model_type
+        self.is_conditioned = is_conditioned
         self.device = "cuda" if torch.cuda.is_available() else "cpu"
         self.betas = torch.linspace(min_beta, max_beta, n_steps).to(self.device)
         self.model = model
         self.alphas = 1 - self.betas
         self.alpha_hats = torch.cumprod(self.alphas, dim=0)
-        self.prev_alphas = F.pad(self.alpha_hats[:-1], (1, 0), value = 1.)
-        self.beta_hats = self.betas * (1. - self.prev_alphas) / (1. - self.alpha_hats)
+        self.prev_alpha_hats = F.pad(self.alpha_hats[:-1], (1, 0), value=1.)
+        self.beta_hats = self.betas * (1. - self.prev_alpha_hats) / (1. - self.alpha_hats)
+
+    def get_statistics(self, x_start, x_t, t):
+        var = self.beta_hats[t][:, None, None, None]
+        alpha_hat = self.alpha_hats[t][:, None, None, None]
+        prev_alphas_hat = self.prev_alpha_hats[t][:, None, None, None]
+        beta_hat = self.beta_hats[t][:, None, None, None]
+
+        b = (x_t - alpha_hat.sqrt() * x_start) / (1. - alpha_hat).sqrt()
+        mean = prev_alphas_hat.sqrt() * x_start + (1. - prev_alphas_hat - beta_hat).sqrt() * b
+        return mean, torch.log(var.clamp(min=1e-20))
 
     def forward_process(self, x, t, noise):
         a_hats = self.alpha_hats[t].reshape(len(x), 1, 1, 1)
         return a_hats.sqrt() * x + (1 - a_hats).sqrt() * noise
 
+    def sample_prev(self, x_t, t, i, w, labels):
+        if self.type == TargetType.NOISE:
+            predicted_noise = self.model(x_t, t)
+            if (w > 0) and (labels is not None):
+                predicted_noise2 = self.model(x_t, t, labels)
+                predicted_noise = (w + 1) * predicted_noise2 - w * predicted_noise
+
+            alpha = self.alphas[i]
+            alpha_hat = self.alpha_hats[i]
+            beta = self.betas[i]
+            if i >= 1:
+                noise = torch.randn_like(x_t)
+            else:
+                noise = 0
+
+            return 1 / torch.sqrt(alpha) * (
+                        x_t - ((1 - alpha) / (torch.sqrt(1 - alpha_hat))) * predicted_noise) + torch.sqrt(beta) * noise
+
+        else:
+            x_start = self.model(x_t, t).clamp_(-1., 1.)
+            mean, var = self.get_statistics(x_start, x_t, t.view(-1))
+            if i >= 1:
+                noise = torch.randn_like(x_t)
+            else:
+                noise = 0
+
+            return mean + torch.exp(0.5 * var) * noise
+
     def sample(self, n, labels=None, w=3):
-        self.model.eval()
-        with torch.no_grad():
-            x = torch.randn((n, 3, self.img_size, self.img_size)).to(self.device)
-            for i in tqdm(reversed(range(0, self.n_steps))):
-                t = (torch.ones(n, 1) * i).long().to(self.device)
-                predicted_noise = self.model(x, t)
-
-                if (w > 0) and (labels is not None):
-                    predicted_noise2 = self.model(x, t, labels)
-                    predicted_noise = (w + 1) * predicted_noise2 - w * predicted_noise
-
-                alpha = self.alphas[i]
-                alpha_hat = self.alpha_hats[i]
-                beta = self.betas[i]
-                if i >= 1:
-                    noise = torch.randn_like(x)
-                else:
-                    noise = 0
-
-                x = 1 / torch.sqrt(alpha) * (
-                        x - ((1 - alpha) / (torch.sqrt(1 - alpha_hat))) * predicted_noise) + torch.sqrt(
-                    beta) * noise
-
-        x = (x.clamp(-1, 1) + 1) / 2
-        x = (x * 255).type(torch.uint8)
-        return x
-
-    def get_statistics(self, x_start, x_t, t):
-        var = self.beta_hats[t][:, None, None, None]
-        b = (x_t - self.alpha_hats[t].sqrt()[:, None, None, None] * x_start) / (1 - self.alpha_hats[t]).sqrt()[:, None, None, None]
-        mean = self.prev_alphas[t].sqrt()[:, None, None, None] * x_start + (1. - self.prev_alphas[t] - self.beta_hats[t]).sqrt()[:, None, None, None] * b
-        return mean, torch.log(var.clamp(min =1e-20))
-
-    def sample_signal(self, n):
         self.model.eval()
         with torch.no_grad():
             x_t = torch.randn((n, 3, self.img_size, self.img_size)).to(self.device)
             for i in tqdm(reversed(range(0, self.n_steps))):
                 t = (torch.ones(n, 1) * i).long().to(self.device)
-                x_start = self.model(x_t, t).clamp_(-1., 1.)
-                mean, var = self.get_statistics(x_start, x_t, t.view(-1))
-                if i >= 1:
-                    noise = torch.randn_like(x_t)
-                else:
-                    noise = 0
-
-                x_t = mean + torch.exp(0.5 * var) * noise
+                x_t = self.sample_prev(x_t, t, i, w, labels)
 
         x_t = (x_t.clamp(-1, 1) + 1) / 2
         x_t = (x_t * 255).type(torch.uint8)
@@ -79,36 +84,7 @@ class DiffusionModel:
         self.model.train()
         mse = nn.MSELoss()
         optim = torch.optim.Adam(self.model.parameters(), lr=1e-3)
-
-        for epoch in tqdm(range(n_epochs)):
-            epoch_loss = 0.0
-            for img, _ in tqdm(data_loader, leave=False, desc=f"Epoch {epoch + 1}/{n_epochs}"):
-                x0 = img.to(self.device)
-
-                eps = torch.randn_like(x0).to(self.device)
-                t = torch.randint(0, self.n_steps, (len(x0), 1)).to(self.device).long()
-
-                noisy_imgs = self.forward_process(x0, t, eps)
-                predict = self.model(noisy_imgs, t)
-
-                if self.predict_noise:
-                    target = eps
-                else:
-                    target = x0
-
-                loss = mse(predict, target)
-                optim.zero_grad()
-                loss.backward()
-                optim.step()
-
-                epoch_loss += loss.item() * len(x0) / dataset_len
-
-            print(f"Loss at epoch {epoch + 1}: {epoch_loss:.3f}")
-
-    def train_cond(self, n_epochs, data_loader, dataset_len):
-        self.model.train()
-        mse = nn.MSELoss()
-        optim = torch.optim.Adam(self.model.parameters(), lr=1e-3)
+        losses = []
 
         for epoch in tqdm(range(n_epochs)):
             epoch_loss = 0.0
@@ -119,13 +95,13 @@ class DiffusionModel:
                 eps = torch.randn_like(x0).to(self.device)
                 t = torch.randint(0, self.n_steps, (len(x0), 1)).to(self.device).long()
 
-                if np.random.random() < 0.1:
+                if not self.is_conditioned or np.random.random() < 0.1:
                     y = None
 
                 noisy_imgs = self.forward_process(x0, t, eps)
                 predict = self.model(noisy_imgs, t, y)
-
-                loss = mse(predict, eps)
+                target = eps if self.type == TargetType.NOISE else x0
+                loss = mse(predict, target)
                 optim.zero_grad()
                 loss.backward()
                 optim.step()
@@ -133,3 +109,6 @@ class DiffusionModel:
                 epoch_loss += loss.item() * len(x0) / dataset_len
 
             print(f"Loss at epoch {epoch + 1}: {epoch_loss:.3f}")
+            losses.append(epoch_loss)
+
+        return losses
--- models.py
+++ models.py
@@ -13,9 +13,9 @@ class Resnet(nn.Module):
                                   nn.Conv2d(out_channels, out_channels, kernel_size=3, padding=1))
 
         if in_channels != out_channels:
-            self.downsample = nn.Conv2d(in_channels, out_channels, kernel_size=1)
+            self.sample = nn.Conv2d(in_channels, out_channels, kernel_size=1)
         else:
-            self.downsample = None
+            self.sample = None
 
         self.t = nn.Sequential(
             nn.SiLU(),
@@ -26,37 +26,71 @@ class Resnet(nn.Module):
         h = self.enc1(x) + self.t(t).reshape(len(x), -1, 1, 1)
         h = self.enc2(h)
 
-        if self.downsample:
-            x = self.downsample(x)
+        if self.sample:
+            x = self.sample(x)
 
         return h + x
 
 
 class SelfAttention(nn.Module):
-    def __init__(self, channels, size):
-        super(SelfAttention, self).__init__()
+    def __init__(self, channels, img_size):
+        super().__init__()
         self.channels = channels
-        self.size = size
+        self.size = img_size
         self.mha = nn.MultiheadAttention(channels, 4, batch_first=True)
         self.ln = nn.LayerNorm([channels])
-        self.ff_self = nn.Sequential(
+        self.mlp = nn.Sequential(
             nn.LayerNorm([channels]),
             nn.Linear(channels, channels),
             nn.SiLU(),
             nn.Linear(channels, channels),
         )
 
-    def forward(self, x, t):
+    def forward(self, x):
         x = x.view(-1, self.channels, self.size * self.size).swapaxes(1, 2)
         x_ln = self.ln(x)
         attention_value, _ = self.mha(x_ln, x_ln, x_ln)
-        attention_value = attention_value + x
-        attention_value = self.ff_self(attention_value) + attention_value
+        attention_value += x
+        attention_value = self.mlp(attention_value) + attention_value
         return attention_value.swapaxes(2, 1).view(-1, self.channels, self.size, self.size)
 
 
+class Encode(nn.Module):
+    def __init__(self, in_channels, out_channels, img_size, time_emb_dim, with_attention=False):
+        super().__init__()
+        self.enc = Resnet(in_channels, out_channels, time_emb_dim)
+        self.att = SelfAttention(out_channels, img_size) if with_attention else None
+        self.enc2 = Resnet(out_channels, out_channels, time_emb_dim)
+        self.att2 = SelfAttention(out_channels, img_size) if with_attention else None
+        self.pool = nn.Conv2d(out_channels, out_channels, kernel_size=2, stride=2)
+
+    def forward(self, x, t):
+        x = self.enc(x, t)
+        x = self.enc2(self.att(x), t) if self.att is not None else self.enc2(x, t)
+        x = self.pool(self.att2(x)) if self.att2 is not None else self.pool(x)
+
+        return x
+
+
+class Decode(nn.Module):
+    def __init__(self, in_channels, out_channels, img_size, time_emb_dim, with_attention=False):
+        super().__init__()
+        self.unpool = nn.ConvTranspose2d(in_channels, out_channels, kernel_size=2, stride=2)
+        self.dec = Resnet(in_channels, out_channels, time_emb_dim)
+        self.att = SelfAttention(out_channels, img_size) if with_attention else None
+        self.dec2 = Resnet(out_channels, out_channels, time_emb_dim)
+        self.att2 = SelfAttention(out_channels, img_size) if with_attention else None
+
+    def forward(self, x, e, t):
+        x = self.unpool(x)
+        x = self.dec(torch.cat([x, e], dim=1), t)
+        x = self.dec2(self.att(x), t) if self.att is not None else self.dec2(x, t)
+        x = self.att2(x) if self.att2 else x
+        return x
+
+
 class UNet(nn.Module):
-    def __init__(self, in_channels, out_channels, n_steps=1000, dim=16, n_classes=None):
+    def __init__(self, in_channels, out_channels, n_steps=1000, dim=16, img_size=16, n_classes=None):
         super().__init__()
 
         self.time_embed = nn.Embedding(n_steps, dim)
@@ -72,53 +106,28 @@ class UNet(nn.Module):
             self.c = nn.Sequential(nn.Linear(dim, time_emb_dim),
                                    nn.SiLU(), nn.Linear(time_emb_dim, time_emb_dim))
 
-        self.enc1 = nn.Conv2d(in_channels, 16, kernel_size=3, padding=1)
+        self.enc1 = nn.Conv2d(in_channels, dim, kernel_size=3, padding=1)
 
-        self.enc2 = Resnet(16, 32, time_emb_dim)
-        self.enc22 = Resnet(32, 32, time_emb_dim)
-        self.pool2 = nn.Conv2d(32, 32, kernel_size=2, stride=2)
+        self.enc2 = Encode(dim, dim * 2, img_size, time_emb_dim)
+        self.enc3 = Encode(dim * 2, dim * 4, img_size // 2, time_emb_dim, with_attention=True)
+        self.enc4 = Encode(dim * 4, dim * 8, img_size // 4, time_emb_dim, with_attention=True)
 
-        self.enc3 = Resnet(32, 64, time_emb_dim)
-        self.att1 = SelfAttention(64, 8)
-        self.enc32 = Resnet(64, 64, time_emb_dim)
-        self.att2 = SelfAttention(64, 8)
-        self.pool3 = nn.Conv2d(64, 64, kernel_size=2, stride=2)
+        self.b1 = Resnet(dim * 8, dim * 16, time_emb_dim)
+        self.att1 = SelfAttention(dim * 16, img_size // 8)
+        self.b2 = Resnet(dim * 16, dim * 16, time_emb_dim)
+        self.att2 = SelfAttention(dim * 16, img_size // 8)
+        self.b3 = Resnet(dim * 16, dim * 8, time_emb_dim)
 
-        self.enc4 = Resnet(64, 128, time_emb_dim)
-        self.att3 = SelfAttention(128, 4)
-        self.enc42 = Resnet(128, 128, time_emb_dim)
-        self.att4 = SelfAttention(128, 4)
-        self.pool4 = nn.Conv2d(128, 128, kernel_size=2, stride=2)
+        self.dec1 = Decode(dim * 8, dim * 4, img_size // 4, time_emb_dim, with_attention=True)
+        self.dec2 = Decode(dim * 4, dim * 2, img_size // 2, time_emb_dim, with_attention=True)
+        self.dec3 = Decode(dim * 2, dim, img_size, time_emb_dim)
 
-        self.b1 = Resnet(128, 256, time_emb_dim)
-        self.b2 = SelfAttention(256, 2)
-        self.b3 = Resnet(256, 256, time_emb_dim)
-        self.b4 = SelfAttention(256, 2)
-        self.b5 = Resnet(256, 128, time_emb_dim)
-
-        self.unpool1 = nn.ConvTranspose2d(128, 64, kernel_size=2, stride=2)
-        self.dec1 = Resnet(128, 64, time_emb_dim)
-        self.att5 = SelfAttention(64, 4)
-        self.dec12 = Resnet(64, 64, time_emb_dim)
-        self.att6 = SelfAttention(64, 4)
-
-        self.unpool2 = nn.ConvTranspose2d(64, 32, kernel_size=2, stride=2)
-        self.dec2 = Resnet(64, 32, time_emb_dim)
-        self.att7 = SelfAttention(32, 8)
-        self.dec22 = Resnet(32, 32, time_emb_dim)
-        self.att8 = SelfAttention(32, 8)
-
-        self.unpool3 = nn.ConvTranspose2d(32, 16, kernel_size=2, stride=2)
-        self.dec3 = Resnet(32, 16, time_emb_dim)
-        self.dec32 = Resnet(16, 16, time_emb_dim)
-
-        self.outconv = nn.Sequential(nn.GroupNorm(1, 16), nn.SiLU(),
-                                     nn.Conv2d(16, out_channels, kernel_size=1))
+        self.outconv = nn.Sequential(nn.GroupNorm(1, dim), nn.SiLU(),
+                                     nn.Conv2d(dim, out_channels, kernel_size=1))
 
     def sinusoidal_embedding(self, n, d):
         embedding = torch.zeros(n, d)
-        wk = torch.tensor([1 / 10_000 ** (2 * j / d) for j in range(d)])
-        wk = wk.reshape((1, d))
+        wk = torch.tensor([1 / 10_000 ** (2 * j / d) for j in range(d)]).reshape((1, d))
         t = torch.arange(n).reshape((n, 1))
         embedding[:, ::2] = torch.sin(t * wk[:, ::2])
         embedding[:, 1::2] = torch.cos(t * wk[:, ::2])
@@ -136,34 +145,16 @@ class UNet(nn.Module):
         e1 = self.enc1(x)
 
         e2 = self.enc2(e1, t)
-        e2 = self.enc22(e2, t)
-        e2 = self.pool2(e2)
-
         e3 = self.enc3(e2, t)
-        e3 = self.enc32(self.att1(e3, t), t)
-        e3 = self.pool3(self.att2(e3, t))
-
         e4 = self.enc4(e3, t)
-        e4 = self.enc42(self.att3(e4, t), t)
-        e4 = self.pool4(self.att4(e4, t))
 
         e5 = self.b1(e4, t)
-        e5 = self.b2(e5, t)
-        e5 = self.b3(e5, t)
-        e5 = self.b4(e5, t)
-        e5 = self.b5(e5, t)
+        e5 = self.b2(self.att1(e5), t)
+        e5 = self.b3(self.att2(e5), t)
 
-        d1 = self.unpool1(e5)
-        d1 = self.dec1(torch.cat([d1, e3], dim=1), t)
-        d1 = self.dec12(self.att5(d1, t), t)
-
-        d2 = self.unpool2(self.att6(d1, t))
-        d2 = self.dec2(torch.cat([d2, e2], dim=1), t)
-        d2 = self.dec22(self.att7(d2, t), t)
-
-        d3 = self.unpool3(self.att8(d2, t))
-        d3 = self.dec3(torch.cat([d3, e1], dim=1), t)
-        d3 = self.dec32(d3, t)
+        d1 = self.dec1(e5, e3, t)
+        d2 = self.dec2(d1, e2, t)
+        d3 = self.dec3(d2, e1, t)
 
         out = self.outconv(d3)
 
